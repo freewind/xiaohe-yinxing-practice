cf0d19447edb59aad84da06ed5d0dfa88b9f4648
freewind <nowindlee@gmail.com>
2018-08-02 14:19:45 +0800

    can check

--- src/main/kotlin/github/freewind/xiaohe/practice/Practice.kt
+++ src/main/kotlin/github/freewind/xiaohe/practice/Practice.kt
@@ -1,36 +1,63 @@
 package github.freewind.xiaohe.practice
 
+import github.freewind.xiaohe.chardecoder.XiaoHeCharDecoder
+import github.freewind.xiaohe.chardecoder.allCharCodes
 import github.freewind.xiaohe.practice.HelloWorldStyle.Companion.row
 import javafx.geometry.Pos
 import javafx.scene.control.Label
 import javafx.scene.image.Image
+import javafx.scene.input.KeyCode
 import javafx.scene.layout.Priority
 import javafx.scene.layout.VBox
 import tornadofx.*
 import java.io.InputStream
+import java.util.*
 
 class HelloWorld : View() {
 
     private var charLabel by singleAssign<Label>()
+    private var tipLabel by singleAssign<Label>()
 
     override val root = vbox {
-        hbox {
+        vbox {
             addClass(row)
             label("我") {
                 charLabel = this
                 addClass(HelloWorldStyle.charLabel)
             }
+            label { tipLabel = this }
             VBox.setVgrow(this, Priority.ALWAYS)
         }
         hbox {
             addClass(row)
             textfield {
                 addClass(HelloWorldStyle.input)
+                setOnKeyReleased { event ->
+                    val code = this.text
+                    if (code.length == 4 || event.code == KeyCode.SPACE) {
+                        checkInput(charLabel.text.single(), code)
+                        this.text = ""
+                    }
+                }
             }
         }
         imageview {
             image = Image(resourceStream("hexing.png")!!)
         }
+    }
+
+    private fun checkInput(char: Char, code: String) {
+        val found = XiaoHeCharDecoder.findCode(char)!!
+        if (found.code.equals(code, true)) {
+            charLabel.text = nextChar().toString()
+            tipLabel.text = ""
+        } else {
+            tipLabel.text = found.code
+        }
+    }
+
+    private fun nextChar(): Char {
+        return allCharCodes.get(Random().nextInt(allCharCodes.size)).char
     }
 
     private fun resourceStream(path: String): InputStream? {
