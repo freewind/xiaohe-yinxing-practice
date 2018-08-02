a4ef9c54f02ca409b566a8f4009b4e19e258393d
freewind <nowindlee@gmail.com>
2018-08-02 14:06:19 +0800

    ui ok

--- src/main/kotlin/github/freewind/xiaohe/practice/Practice.kt
+++ src/main/kotlin/github/freewind/xiaohe/practice/Practice.kt
@@ -1,28 +1,67 @@
 package github.freewind.xiaohe.practice
 
+import github.freewind.xiaohe.practice.HelloWorldStyle.Companion.row
+import javafx.geometry.Pos
+import javafx.scene.control.Label
+import javafx.scene.image.Image
+import javafx.scene.layout.Priority
+import javafx.scene.layout.VBox
 import tornadofx.*
+import java.io.InputStream
 
 class HelloWorld : View() {
-    private val label = label("Hello, TornadoFX!")
+
+    private var charLabel by singleAssign<Label>()
 
     override val root = vbox {
-        menubar {
-            menu("Menu") {
-                item("Change Text").setOnAction { _ ->
-                    label.text = "Changed"
-                }
+        hbox {
+            addClass(row)
+            label("我") {
+                charLabel = this
+                addClass(HelloWorldStyle.charLabel)
+            }
+            VBox.setVgrow(this, Priority.ALWAYS)
+        }
+        hbox {
+            addClass(row)
+            textfield {
+                addClass(HelloWorldStyle.input)
             }
         }
-        add(label)
+        imageview {
+            image = Image(resourceStream("hexing.png")!!)
+        }
+    }
+
+    private fun resourceStream(path: String): InputStream? {
+        return this.javaClass.classLoader.getResourceAsStream(path)
     }
 }
 
 class HelloWorldStyle : Stylesheet() {
+    companion object {
+        val row by cssclass()
+        val input by cssclass()
+        val charLabel by cssclass()
+    }
+
     init {
         root {
-            prefWidth = 400.px
-            prefHeight = 400.px
-            fontSize = 20.px
+            prefWidth = 600.px
+            prefHeight = 800.px
+            alignment = Pos.CENTER
+            spacing = 20.px
+            padding = box(20.px)
+        }
+        charLabel {
+            fontSize = 300.px
+        }
+        row {
+            alignment = Pos.CENTER
+        }
+        input {
+            prefWidth = 200.px
+            fontSize = 40.px
         }
     }
 }
